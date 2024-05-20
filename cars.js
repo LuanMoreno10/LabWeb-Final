50b4e82a3fbf4dbc0da20a1cfd118df054153633
IPedroCostaI <43112@ufp.edu.pt>
2024-05-20 23:45:57 +0100

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,10 @@
+// cars.js
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close the modal when clicking
